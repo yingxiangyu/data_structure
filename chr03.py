34d0cd986570dc1ce7ccd73dce4e942e7bf8eb9a
yyx <yingxiang.yu.cn@gmail.com>
2019-05-07 20:30:56 +0800

    cha03

--- chr03.py
+++ chr03.py
@@ -38,30 +38,35 @@ class List:
             return None
 
     def insertAsFirst(self, e):
-        p = ListNode(data=e)
-        p.succ = self.header.succ
-        self.header.succ = p
-        p.pred = self.header
+        self.insertAsSucc(self.header, e)
 
     def insertAsLast(self, e):
-        p = ListNode(data=e)
-        p.pred = self.trailer.pred
-        self.trailer.pred = p
-        p.succ = self.trailer
+        self.insertAsPred(self.trailer, e)
 
-    @staticmethod
-    def insertAsPred(p, e):  # 作为前驱节点插入
-        n = ListNode(data=e)
-        n.pred = p.pred
+    def insertAsPred(self, p, e):  # 作为前驱节点插入
+        if p not in self:
+            raise ValueError('p not in List')
+        n = ListNode(data=e, pred=p.pred, succ=p)
+        p.pred.succ = n
         p.pred = n
-        n.succ = p
+        self._size += 1
 
-    @staticmethod
-    def insertAsSucc(p, e):  # 作为后继节点插入
-        n = ListNode(data=e)
-        n.succ = p.succ
+    def insertAsSucc(self, p, e):  # 作为后继节点插入
+        if p not in self:
+            raise ValueError('p not in List')
+        n = ListNode(data=e, pred=p, succ=p.succ)
+        p.succ.pred = n
         p.succ = n
-        n.pred = p
+        self._size += 1
+
+    def __contains__(self, item):
+        p = self.header.succ
+        while p != self.trailer:
+            if p == item:
+                return True
+            else:
+                p = p.succ
+        return False
 
     def __getitem__(self, r):
         p = self.first()
@@ -78,10 +83,49 @@ class List:
     def size(self):
         return self._size
 
-    @staticmethod
-    def remove(p):
+    def remove(self, p):
+        if p not in self:
+            raise ValueError('p not in List')
+
         p.pred.succ = p.succ
         p.succ.pred = p.pred
+        self._size-=1
         return p.data
 
-    
+    def disordered(self):  # 逆序对
+        n = 0
+        p = self.header.succ
+        for i in range(self._size):
+            if p.succ.data >= p.data:
+                n += 1
+            p = p.succ
+        return 0
+
+    def find(self, e, n, p):  # 在无序列表内节点p（可能是trailer）的n个（真）前驱中，找到等于e的最后者
+        while n > 0 and p != self.header:
+            if p.data == e:
+                return p
+            else:
+                p = p.pred
+                n -= 1
+        return None
+
+    def copyNodes(self, p, n):
+        self.trailer.pred = self.header
+        self.header.succ = self.trailer
+        self._size = 0
+        while n:
+            self.insertAsLast(p.data)
+            if p.succ.succ :
+                p = p.succ
+            else:
+                import warnings
+                warnings.warn('节点数量不足', DeprecationWarning)
+                break
+            n -= 1
+
+    def clear(self):
+        self.trailer.pred = self.header
+        self.header.succ = self.trailer
+        self._size = 0
+
